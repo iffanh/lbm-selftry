5ce57d33085de3a93296a897d59f2e25d775093a
Muhammad Iffan Hannanu <muhammih@stud.ntnu.no>
2018-10-05 10:25:35 +0200

    more flexibility

--- lbm_fluxBC.py
+++ lbm_fluxBC.py
@@ -6,7 +6,7 @@ import math
 import numpy as np
 import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
-import lbm_input as ini
+import lbm_input as ini             #import from theinput file
 
 ####################################################### SIMULATION ###########################################################################
 
@@ -15,7 +15,7 @@ for t in range(ini.T):
     #Zou and He velocity BCs on west side
     for j in range(1,ini.sizeY_+1):
         for i in range(1,ini.sizeX_+1):
-            if ini.solid[i][j] == 2:
+            if ini.m[i][j] == 2:
                 ini.rho[i][j] = (ini.f[i][j][0] + ini.f[i][j][2] + ini.f[i][j][4] + 2.*(ini.f[i][j][3] + ini.f[i][j][7] + ini.f[i][j][6])) / (1 - ini.ux0)
                 ru = ini.rho[i][j]*ini.ux0
                 ini.f[i][j][1] = ini.f[i][j][3] + (2./3.)*ru
@@ -26,7 +26,7 @@ for t in range(ini.T):
     # ... computing density for imaging        
     for j in range(1,ini.sizeY_+1):
         for i in range(1,ini.sizeX_+1):
-            if ini.solid[i][j] == 0:
+            if ini.m[i][j] == 0:
                 ini.rho[i][j] = 0
                 for a in range(9):
                     ini.ftemp[i][j][a] = 0
@@ -40,40 +40,40 @@ for t in range(ini.T):
         for i in range(1,ini.sizeX_+ 1):
             i_n = (i-1) 
             i_p = (i+1)
-            if (ini.solid[i][j] <> 1):
-                #For streaming part, if adjacent grid is a solid, then the density distribution will propagate, 
+            if (ini.m[i][j] <> 1):
+                #For streaming part, if adjacent grid is a m, then the density distribution will propagate, 
                 #Else, the density is bounced back to the same grid, but with different direction
                 
                 ini.ftemp[i][j][0] = ini.f[i][j][0]
                 
-                if (ini.solid[i_p][j] <> 1):    ini.ftemp[i_p][j][1] = ini.f[i][j][1]#; f[i][j][1] = 0
+                if (ini.m[i_p][j] <> 1):    ini.ftemp[i_p][j][1] = ini.f[i][j][1]#; f[i][j][1] = 0
                 else:                           ini.ftemp[i][j][3] = ini.f[i][j][1]#; f[i][j][1] = 0
 
-                if (ini.solid[i][j_p] <> 1):    ini.ftemp[i][j_p][2] = ini.f[i][j][2]#; f[i][j][2] = 0
+                if (ini.m[i][j_p] <> 1):    ini.ftemp[i][j_p][2] = ini.f[i][j][2]#; f[i][j][2] = 0
                 else:                           ini.ftemp[i][j][4] = ini.f[i][j][2]#; f[i][j][2] = 0
 
-                if (ini.solid[i_n][j] <> 1):    ini.ftemp[i_n][j][3] = ini.f[i][j][3]#; f[i][j][3] = 0 
+                if (ini.m[i_n][j] <> 1):    ini.ftemp[i_n][j][3] = ini.f[i][j][3]#; f[i][j][3] = 0 
                 else:                           ini.ftemp[i][j][1] = ini.f[i][j][3]#; f[i][j][3] = 0 
 
-                if (ini.solid[i][j_n] <> 1):    ini.ftemp[i][j_n][4] = ini.f[i][j][4]#; f[i][j][4] = 0
+                if (ini.m[i][j_n] <> 1):    ini.ftemp[i][j_n][4] = ini.f[i][j][4]#; f[i][j][4] = 0
                 else:                           ini.ftemp[i][j][2] = ini.f[i][j][4]#; f[i][j][4] = 0
 
-                if (ini.solid[i_p][j_p] <> 1):  ini.ftemp[i_p][j_p][5] = ini.f[i][j][5]#; f[i][j][5] = 0
+                if (ini.m[i_p][j_p] <> 1):  ini.ftemp[i_p][j_p][5] = ini.f[i][j][5]#; f[i][j][5] = 0
                 else:                           ini.ftemp[i][j][7] = ini.f[i][j][5]#; f[i][j][5] = 0
 
-                if (ini.solid[i_n][j_p] <> 1):  ini.ftemp[i_n][j_p][6] = ini.f[i][j][6]#; f[i][j][6] = 0
+                if (ini.m[i_n][j_p] <> 1):  ini.ftemp[i_n][j_p][6] = ini.f[i][j][6]#; f[i][j][6] = 0
                 else:                           ini.ftemp[i][j][8] = ini.f[i][j][6]#; f[i][j][6] = 0
 
-                if (ini.solid[i_n][j_n] <> 1):  ini.ftemp[i_n][j_n][7] = ini.f[i][j][7]#; f[i][j][7] = 0
+                if (ini.m[i_n][j_n] <> 1):  ini.ftemp[i_n][j_n][7] = ini.f[i][j][7]#; f[i][j][7] = 0
                 else:                           ini.ftemp[i][j][5] = ini.f[i][j][7]#; f[i][j][7] = 0
 
-                if (ini.solid[i_p][j_n] <> 1):  ini.ftemp[i_p][j_n][8] = ini.f[i][j][8]#; f[i][j][8] = 0
+                if (ini.m[i_p][j_n] <> 1):  ini.ftemp[i_p][j_n][8] = ini.f[i][j][8]#; f[i][j][8] = 0
                 else:                           ini.ftemp[i][j][6] = ini.f[i][j][8]#; f[i][j][8] = 0
     
     # ... and then computing macroscopic density and velocity for each lattice point, after shifting        
     for j in range(1,ini.sizeY_+1):
         for i in range(1,ini.sizeX_+1):
-            if ini.solid[i][j] <> 1:# or solid[i][j] == 1:
+            if ini.m[i][j] <> 1:# or m[i][j] == 1:
                 ini.rho[i][j] = 0
                 ini.ux[i][j] = 0
                 ini.uy[i][j] = 0
@@ -87,7 +87,7 @@ for t in range(ini.T):
     #Computing equilibrium distribution function
     for j in range(1,ini.sizeY_+ 1):
         for i in range(1,ini.sizeX_+ 1):
-            if ini.solid[i][j] == 0:
+            if ini.m[i][j] == 0:
                 fct1 = ini.w1*ini.rho[i][j]
                 fct2 = ini.w2*ini.rho[i][j]
                 fct3 = ini.w3*ini.rho[i][j]
@@ -122,7 +122,7 @@ for t in range(ini.T):
         for i in range(1,ini.sizeX_+1):
             i_n = (i-1) 
             i_p = (i+1)
-            if ini.solid[i][j] == 0:
+            if ini.m[i][j] == 0:
                 for a in range(9):
                     #Only calculate the one with no bounce back? Not yet implemented
                     ini.f[i][j][a] = ini.ftemp[i][j][a] - (ini.ftemp[i][j][a] - ini.feq[i][j][a]) / ini.tau
--- lbm_input.py
+++ lbm_input.py
@@ -1,13 +1,22 @@
 ####################################################### PREPARATION ###########################################################################
+import numpy as np
+
+#importing grid
+m = np.genfromtxt('matrix_structure.dat', delimiter=" ")
+m = zip(*m)
+
 #The size of grid
-sizeX_ = 40         #length in x-direction
-sizeY_ = 16         #length in y-direction
+# sizeX_ = 40         #length in x-direction
+# sizeY_ = 16         #length in y-direction
+
+sizeX_ = len(m) - 2         #length in x-direction
+sizeY_ = len(m[0]) - 2        #length in y-direction
 
 #The number of iteration
 T = 200            #Total time used in the simulation
 dt = 1             #time interval
 
-solid = [[0 for j in xrange(sizeY_ + 2)] for i in xrange(sizeX_ + 2)]                       #Presence of solid or not, 1 means solid
+#m = [[0 for j in xrange(sizeY_ + 2)] for i in xrange(sizeX_ + 2)]                       #Presence of m or not, 1 means m
 
 #Declaring variables
 
@@ -35,44 +44,37 @@ f3 = 3./2.
 f_tol = 0. 
 
 ####################################################### FUNCTIONS ###########################################################################
-#To find out whether a solid is in contact in fluid or not
+#To find out whether a m is in contact in fluid or not
 def is_interior(i,j):       
-    if solid[i][j] == 1 and solid[i-1][j] == 1 and solid[i+1][j] == 1 and solid[i][j-1] == 1 and solid[i][j+1] == 1 and solid[i-1][j-1] == 1 and solid[i+1][j-1] == 1 and solid[i-1][j+1] == 1 and solid[i+1][j+1] == 1:
+    if m[i][j] == 1 and m[i-1][j] == 1 and m[i+1][j] == 1 and m[i][j-1] == 1 and m[i][j+1] == 1 and m[i-1][j-1] == 1 and m[i+1][j-1] == 1 and m[i-1][j+1] == 1 and m[i+1][j+1] == 1:
         return True
 
 ####################################################### INTIALIZATION ###########################################################################
 
-####Sacrificed grid 
-for i in range(0,sizeX_+ 2):
-    for j in range(0,sizeY_ + 2):
-        solid[i][0] = 0
-        solid[i][sizeY_+1] = 0
-        solid[0][j] = 0
-        solid[sizeX_+1][j] = 0
+# ####Sacrificed grid 
+# for i in range(0,sizeX_+ 2):
+#     for j in range(0,sizeY_ + 2):
+#         m[i][0] = 0
+#         m[i][sizeY_+1] = 0
+#         m[0][j] = 0
+#         m[sizeX_+1][j] = 0
 
-####Solid presence in Grid 
-#The outer boundary
-#0 is a normal no solid boundary
-#1 is a bounce back boundary
-#2 is a von neumann boundary
-for i in range(1,sizeX_+ 1):
-    for j in range(1,sizeY_ + 1):
-        solid[i][1] = 1
-        solid[i][sizeY_] = 1
-        solid[1][j] = 2
-        solid[sizeX_][j] = 0
+# ####m presence in Grid 
+# #The outer boundary
+# #0 is a normal no solid boundary
+# #1 is a bounce back boundary
+# #2 is a von neumann boundary from the left side
+# for i in range(1,sizeX_+ 1):
+#     for j in range(1,sizeY_ + 1):
+#         m[i][1] = 1
+#         m[i][sizeY_] = 1
+#         m[1][j] = 2
+#         m[sizeX_][j] = 0
 
-solid[1][1] = 1
-solid[1][sizeY_] = 1
-solid[sizeX_][1] = 1
-solid[sizeX_][sizeY_] = 1
-
-#The cylinder
-solid[7][10] = 1; solid[8][10] = 1; solid[9][10] = 1
-solid[6][9] = 1; solid[7][9] = 1; solid[8][9] = 1; solid[9][9] = 1; solid[10][9] = 1
-solid[6][8] = 1; solid[7][8] = 1; solid[8][8] = 1; solid[9][8] = 1; solid[10][8] = 1
-solid[6][7] = 1; solid[7][7] = 1; solid[8][7] = 1; solid[9][7] = 1; solid[10][7] = 1
-solid[7][6] = 1; solid[8][6] = 1; solid[9][6] = 1
+# # m[1][1] = 1
+# # m[1][sizeY_] = 1
+# # m[sizeX_][1] = 1
+# # m[sizeX_][sizeY_] = 1
 
 ####Initial Condition for density distribution, f
 #Initialize density distribution f, ...
@@ -80,16 +82,16 @@ solid[7][6] = 1; solid[8][6] = 1; solid[9][6] = 1
 f_init = 0.1
 for j in range(1,sizeY_+ 1):
     for i in range(1, sizeX_+ 1):                 
-        if solid[i][j] == 0:
+        if m[i][j] == 0:
             for a in range(9):
                 f[i][j][a] = f_init 
 
 ###Von Neumann Boundary condition
 #Initializing flux boundary density distribution
-ux0 = 0.8
+ux0 = 0.6
 for i in range(1,sizeX_+ 1):
     for j in range(1,sizeY_ + 1):
-        if solid[i][j] == 2:
+        if m[i][j] == 2:
             #West side
             for a in [0,2,3,4,6,7]:
                 f[i][j][a] = f_init
